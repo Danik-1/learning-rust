67bbd7dcbe1191b4fe9c8f2455b07ca80daa8e2f
Danik-1 <lyatorovskiy@gmail.com>
2023-01-28 14:28:51 +0300

    Previous files deleted Now program built with cargo in tutorial folder

---
+++ tutorial/target/debug/deps/tutorial.d
@@ -0,0 +1,5 @@
+C:\Users\danya\Desktop\proga\mine\rust\learning rust\tutorial\target\debug\deps\tutorial.exe: src\main.rs
+
+C:\Users\danya\Desktop\proga\mine\rust\learning rust\tutorial\target\debug\deps\tutorial.d: src\main.rs
+
+src\main.rs:
---
+++ tutorial/target/debug/tutorial.d
@@ -0,0 +1,1 @@
+C:\Users\danya\Desktop\proga\mine\rust\learning\ rust\tutorial\target\debug\tutorial.exe: C:\Users\danya\Desktop\proga\mine\rust\learning\ rust\tutorial\src\main.rs
